3b9101f4082ddf29c4fe4d500c5e2fcc776edbbf
Antonio Saad <antoniosaadkhalil@gmail.com>
2016-07-09 09:34:52 -0430

    Consume test successfull

--- test/queue.js
+++ test/queue.js
@@ -13,8 +13,12 @@ describe('proton-amqp-queue class test', () => {
     const ch = yield conn.createChannel()
     yield ch.assertQueue('AMQPQueueTest', AMQPQueueTest.options)
     const queue = new AMQPQueueTest(ch, 'AMQPQueueTest')
+    const bindings = queue.bindings
+    for(let b in bindings){
+      yield ch.bindQueue(queue.name, bindings[b].source, bindings[b].routingKey, bindings[b].args)
+     }
     yield queue.destroy()
-    // yield queue.closeChannel()
-    // yield conn.close()
+    yield queue.closeChannel()
+    yield conn.close()
   })
 })
